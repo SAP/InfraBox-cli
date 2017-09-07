a5df5951f5ca15e5c31673985cd3fa7c5baa075b
Steffen <steffen@infrabox.net>
2017-09-07 23:02:10 +0200

    version 0.3.1

--- infraboxcli/__init__.py
+++ infraboxcli/__init__.py
@@ -11,7 +11,7 @@ from infraboxcli.log import logger
 from infraboxcli.init import init
 from infraboxcli.pull import pull
 
-version = '0.3.0'
+version = '0.3.1'
 
 def main():
     parser = argparse.ArgumentParser(prog="infrabox")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='infraboxcli',
-      version='0.3.0',
+      version='0.3.1',
       url='https://github.com/infrabox/cli',
       description='Command Line Interface for InfraBox',
       long_description=readme(),
