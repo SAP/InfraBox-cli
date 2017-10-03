85b0f54f4918bbb2989fb60d95e18035369e39e3
Steffen <steffen@infrabox.net>
2017-10-03 13:03:17 +0200

    upload test result

--- infrabox/pyinfrabox/entrypoint.sh
+++ infrabox/pyinfrabox/entrypoint.sh
@@ -1,14 +1,29 @@
 #!/bin/sh -e
-
 mkdir -p /infrabox/upload/badge
 mkdir -p /infrabox/upload/markup
 
 echo "## Run tests"
-nosetests --with-coverage --cover-xml --cover-branches --cover-package=pyinfrabox --cover-tests tests/*
+nosetests \
+    --with-xunit \
+    --with-coverage \
+    --cover-xml \
+    --cover-branches \
+    --cover-package=pyinfrabox \
+    --cover-tests tests/*
 
-echo "## Download coverage"
-git clone https://github.com/InfraBox/coverage.git /tmp/coverage
+echo "## Get InfraBox testresult"
+git clone https://github.com/InfraBox/testresult.git /tmp/infrabox-testresult
+
+echo "## Get InfraBox coverage"
+git clone https://github.com/InfraBox/coverage.git /tmp/infrabox-coverage
 
 echo "## Create coverage result"
 export PYTHONPATH=/
-python /tmp/coverage/coverage.py --output /infrabox/upload/markup/coverage.json --input coverage.xml --badge /infrabox/upload/badge/coverage.json --format py-coverage
+python /tmp/infrabox-coverage/coverage.py \
+    --output /infrabox/upload/markup/coverage.json \
+    --input coverage.xml \
+    --badge /infrabox/upload/badge/coverage.json \
+    --format py-coverage
+
+echo "## Convert test result"
+python /tmp/infrabox-testresult/testresult.py -f xunit -i nosetests.xml
