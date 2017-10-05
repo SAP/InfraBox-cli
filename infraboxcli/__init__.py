6341e012c66d99362e6e8dc18ecf44b5eacad55a
Steffen <steffen@infrabox.net>
2017-10-05 18:03:29 +0200

    inc version 0.3.4

--- infraboxcli/__init__.py
+++ infraboxcli/__init__.py
@@ -11,7 +11,7 @@ from infraboxcli.log import logger
 from infraboxcli.init import init
 from infraboxcli.pull import pull
 
-version = '0.3.3'
+version = '0.3.4'
 
 def main():
     parser = argparse.ArgumentParser(prog="infrabox")
