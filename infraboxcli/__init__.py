44118954c825c40ab14383e973ab0df18dfcff43
Steffen <steffen@infrabox.net>
2018-01-19 01:00:01 +0100

    inc v0.5.7

--- infraboxcli/__init__.py
+++ infraboxcli/__init__.py
@@ -11,7 +11,7 @@ from infraboxcli.log import logger
 from infraboxcli.init import init
 from infraboxcli.pull import pull
 
-version = '0.5.6'
+version = '0.5.7'
 
 def main():
     username = 'unknown'
