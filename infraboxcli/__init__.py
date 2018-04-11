998959b7e1830e2169289e97fdaad82c9d958539
Kirill Abramov <gammerxpower@gmail.com>
2018-04-11 15:55:06 +0200

    (ref #30) New CLI API: improvements.
    * Added possibility to specify needed memory and cpu consumptions for
      `infrabox run` command;
    * New limits taken from the command line args are applied to each job
      taken from current infrabox.json file.

--- infraboxcli/__init__.py
+++ infraboxcli/__init__.py
@@ -93,7 +93,10 @@ def main():
     parser_run.add_argument("--local-cache", required=False, type=str,
                             default="/tmp/{}/infrabox/local-cache".format(username),
                             help="Path to the local cache")
-
+    parser_run.add_argument("--memory", required=False, type=float,
+                            help="Override a memory consumption for your job")
+    parser_run.add_argument("--cpu", required=False, type=float,
+                            help="Override a cpu consumption for your job")
     parser_run.set_defaults(no_rm=False)
     parser_run.set_defaults(func=run)
 
