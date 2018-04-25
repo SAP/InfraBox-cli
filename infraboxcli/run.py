7edafb4c5faa083c90f2f06de2887d650e7fa4f5
Steffen Geissinger <steffen@infrabox.net>
2018-04-25 14:59:54 +0200

    Revert "Merge pull request #31 from InfraBox/cli_project_features"
    This reverts commit 772128a91f88027bb6fecd681cf83c612a81cd3c, reversing
    changes made to 312d34da013ccd87dedb73dc04610c4a10b4f17a.

--- infraboxcli/run.py
+++ infraboxcli/run.py
@@ -447,15 +447,6 @@ def run(args):
 
     # validate infrabox.json
     data = load_infrabox_json(args.infrabox_json)
-    if args.memory:
-        print('WARNING: only int resource limits are supported right now. Using rounded int instead of provided value.')
-        for job in data['jobs']:
-            job['resources']['limits']['memory'] = int(args.memory)
-    if args.cpu:
-        print('WARNING: only int resource limits are supported right now. Using rounded int instead of provided value.')
-        for job in data['jobs']:
-            job['resources']['limits']['cpu'] = int(args.cpu)
-
     jobs = get_job_list(data, args, infrabox_context=args.project_root)
 
     if not args.job_name:
