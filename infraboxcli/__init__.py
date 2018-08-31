e9ba92620377399175905eb6140bce6d941e6d5a
berry <berry.ban@sap.com>
2018-08-31 17:20:09 +0800

    bump version to 0.7.5

--- infraboxcli/__init__.py
+++ infraboxcli/__init__.py
@@ -17,7 +17,7 @@ from infraboxcli.dashboard import remotes
 from infraboxcli.dashboard import local_config
 from infraboxcli.install import install_infrabox
 
-version = '0.7.4'
+version = '0.7.5'
 
 def main():
     username = 'unknown'
