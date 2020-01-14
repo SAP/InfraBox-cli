98eabc3d3eba5c538b375525f3da6e1dd0bf67ca
Karsten Amrhein <karsten.amrhein@sap.com>
2020-01-14 13:41:53 +0100

    v0.8.6

--- infraboxcli/__init__.py
+++ infraboxcli/__init__.py
@@ -17,7 +17,7 @@ from infraboxcli.dashboard import remotes
 from infraboxcli.dashboard import local_config
 from infraboxcli.install import install_infrabox
 
-version = '0.8.5'
+version = '0.8.6'
 
 def main():
     username = 'unknown'
